409e2194b787fdf6b2dd5d7a536e96c54c0809bf
Ethan Szeto <ethan.szeto@gmail.com>
2023-04-23 00:24:43 -0400

    Added controller files in anticipation for sending articles to client
    Also in anticipation of having a system where certain users with certain permissions have access to creating/editing articles on the site.

--- api/routes/articles.route.js
+++ api/routes/articles.route.js
@@ -1,17 +1,19 @@
-// /* Controls Routing for Finished Articles */
-// /*
-// This will require special implementation because
-// each article needs to be standardized so that
-// we can read it straight off the database and not have to control
-// the entire thing from the file routing system. 
-// */
-// import express from 'express'
-// import path from 'path'
+/* Controls Routing for Finished Articles */
 
-// const router = express.Router();
+/*
+This will require special implementation because
+each article needs to be standardized so that
+we can read it straight off the database and not have to control
+the entire thing from the file routing system. 
 
-// router.route('/').get((req, res) => {
-//     res.sendFile(path.resolve() + '/src/pages/HomePage/index.html');
-// })
+*/
 
-// export default router
+import express from 'express'
+import ArticlesCTRL from './controllers/articles.controller.js'
+import path from 'path'
+
+const router = express.Router();
+
+router.route('/articles/:id').get(ArticlesCTRL.apiGetArticles)
+
+//export default router
