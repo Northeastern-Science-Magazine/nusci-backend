c3fd53aa79b18ae5560322436fdeb6d06abbece0
Ethan Szeto <ethan.szeto@gmail.com>
2023-06-08 19:35:49 -0400

    Connected to MongoDB
    Added mongo client to index.js. Added proper connection to the database.

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 //Entry point file. Getting an endpoint from app
 import app from './api/app.js'
+import 'dotenv/config'
+import mongodb from 'mongodb'
 
-//import mongodb from 'mongodb'
-
-//const MongoClient = mongodb.MongoClient;
-//const mongo_username = proccess.env('MONGO_USERNAME');
-//const mongo_password = proccess.env('MONGO_PASSWORD');
-//const mongo_cluster = proccess.env('MONGO_CLUSTER');
-//const uri = ``;
+const MongoClient = mongodb.MongoClient;
+const mongo_username = process.env['MONGO_USERNAME'];
+const mongo_password = process.env['MONGO_PASSWORD'];
+const mongo_cluster = process.env['MONGO_CLUSTER'];
+const uri = `mongodb+srv://${mongo_username}:${mongo_password}@${mongo_cluster}.xtdufxk.mongodb.net/?retryWrites=true&w=majority`;
 
 const HOSTNAME = '0.0.0.0';
 const PORT = 9998;
 
-app.listen(PORT, HOSTNAME, () => {
-  console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
-});
+MongoClient.connect(
+  uri,
+  {
+    maxPoolSize: 50,
+    wtimeoutMS: 2500,
+    useNewUrlParser: true
+  }).catch(err => {
+    console.error(err.stack)
+    process.exit(1)
+  }).then(async client => {
+    app.listen(PORT, () => {
+      console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+    });
+  })
+
