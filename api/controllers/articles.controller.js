1cb9aca883c1811d8771dbf78dbe59a2ffa28701
Kevin Lu <kevinlu4588@gmail.com>
2023-06-24 20:55:13 -0400

    basic auth -> jwt

--- api/controllers/articles.controller.js
+++ api/controllers/articles.controller.js
@@ -1,1 +1,23 @@
-//controller for finished articles request (on the database)
+// //controller for finished articles request (on the database)
+// import path from "path";
+// import ArticlesAccessor from "../database_accessor/articles_accessor.js";
+
+// export default class ArticlesCTRL {
+//     static async apiGetArticle(req, res, next) {
+//         try {
+//             let id = req.params.id || {};
+
+//             // GET INFORMATION FROM DB HERE ONCE MODELS ARE IMPLEMENTED
+//             let reviews = await ArticlesAccessor.getArticles(id);
+
+//             if (!reviews) {
+//                 return res.status(404).json({ error: "Not found." });
+//             }
+
+//             // will be templated, this is just boilerplate
+//             return res.json({ reviews });
+//         } catch (e) {
+//             return res.status(500).json({ error: "Server error" });
+//         }
+//     }
+// }
