cfd83f00379594955d1fcdd543a267adc367ad5e
Kevin Lu <kevinlu4588@gmail.com>
2023-06-29 12:09:12 -0400

    small bug fix

--- api/database_accessor/articles_accessor.js
+++ api/database_accessor/articles_accessor.js
@@ -1,16 +1,16 @@
 import mongodb from "mongodb";
 
 const OBJECT_ID = new mongodb.ObjectId();
-let reviews;
+let articles;
 
 export default class ArticlesAccessor {
-    static async injectDB(connection) {
-        if (reviews) {
+    static async InjectDB(connection) {
+        if (articles) {
             return;
         }
 
         try {
-            reviews = await connection
+            articles = await connection
                 .db("ArticleDatabase")
                 .collection("ArticleInfo");
         } catch (e) {
@@ -20,7 +20,7 @@ export default class ArticlesAccessor {
 
     static async getArticles(articleId) {
         try {
-            return await reviews.findOne({ _id: OBJECT_ID(articleId) });
+            return await articles.findOne({ _id: OBJECT_ID(articleId) });
         } catch (e) {
             console.error(e);
             return { error: e };
