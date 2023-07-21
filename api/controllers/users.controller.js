70e465a33c6f379220224e18c5188b50578ea3c9
Ethan Szeto <ethan.szeto@gmail.com>
2023-07-21 11:49:02 -0400

    Moved post signup and login to a user controller file.
    What needs to be done is move the actual database connection code to a db accessor file for users.

---
+++ api/controllers/users.controller.js
@@ -0,0 +1,47 @@
+dotenvConfig(); // load .env variables
+import { config as dotenvConfig } from 'dotenv';
+import User from "../models/user.js"; // import user model
+import bcrypt from "bcryptjs"; // import bcrypt to hash passwords
+import jwt from "jsonwebtoken"; // import jwt to sign tokens
+//DESTRUCTURE ENV VARIABLES WITH DEFAULTS
+const { SECRET = "secret" } = process.env;
+
+export default class UserCTRL {
+    static async apiPostLogin(req, res) {
+        try {
+            // check if the user exists
+            const user = await User.findOne({ username: req.body.username });
+            if (user) {
+                //check if password matches
+                const result = await bcrypt.compare(req.body.password, user.password);
+                if (result) {
+                // sign token and send it in response
+                const token = await jwt.sign({ username: user.username }, SECRET);
+                res.json({ token });
+                } else {
+                res.status(400).json({ error: "password doesn't match" });
+                }
+            } else {
+                res.status(400).json({ error: "User doesn't exist" });
+            }
+        } catch (error) {
+            res.status(400).json({ error });
+        }
+    }
+
+    static async apiPostSignUp(req, res) {
+        try {
+            console.log("Sign up username: " + req.body.username);
+            console.log("Sign up password: " + req.body.password);
+            // hash the password
+            req.body.password = await bcrypt.hash(req.body.password, 10);
+            // create a new user in database
+            const user = await User.create(req.body);
+            // send new user as response
+            res.json(user);
+        } catch (error) {
+            console.log(error);
+            res.status(400).json({ error });
+        }
+    }
+}
--- api/routes/pages.route.js
+++ api/routes/pages.route.js
@@ -1,20 +1,12 @@
 import express from 'express'
 import path from 'path'
 import { isLoggedIn } from "../controllers/login_verification.js";
-import { config as dotenvConfig } from 'dotenv';
-
-dotenvConfig(); // load .env variables
-import User from "../models/user.js"; // import user model
-import bcrypt from "bcryptjs"; // import bcrypt to hash passwords
-import jwt from "jsonwebtoken"; // import jwt to sign tokens
-import bodyParser from "body-parser"; //parser to process requests
+import UserCTRL from '../controllers/users.controller.js';
+import bodyParser from 'body-parser';
 
 const router = express.Router();
 
 router.use(bodyParser.urlencoded({ extended: false }));
-
-//DESTRUCTURE ENV VARIABLES WITH DEFAULTS
-const { SECRET = "secret" } = process.env;
 
 /**
  * This file controls the routing for the static
@@ -54,47 +46,13 @@ router.route('/signup')
 .get((req, res) => {
     res.sendFile(path.resolve() + '/public/html/signup.html');
 })
-.post(async (req, res) => {
-  try {
-    console.log("Sign up username: " + req.body.username);
-    console.log("Sign up password: " + req.body.password);
-    // hash the password
-    req.body.password = await bcrypt.hash(req.body.password, 10);
-    // create a new user in database
-    const user = await User.create(req.body);
-    // send new user as response
-    res.json(user);
-  } catch (error){
-    console.log(error);
-    res.status(400).json({ error });
-  }
-});
+.post(UserCTRL.apiPostSignUp);
 
 /* Login Page Router */
 router.route('/login')
 .get((req, res) => {
   res.sendFile(path.resolve() + '/public/html/login.html');
 })
-.post(async (req, res) => {
-  try {
-    // check if the user exists
-    const user = await User.findOne({ username: req.body.username });
-    if (user) {
-      //check if password matches
-      const result = await bcrypt.compare(req.body.password, user.password);
-      if (result) {
-        // sign token and send it in response
-        const token = await jwt.sign({ username: user.username }, SECRET);
-        res.json({ token });
-      } else {
-        res.status(400).json({ error: "password doesn't match" });
-      }
-    } else {
-      res.status(400).json({ error: "User doesn't exist" });
-    }
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-});
+.post(UserCTRL.apiPostLogin);
 
 export default router
