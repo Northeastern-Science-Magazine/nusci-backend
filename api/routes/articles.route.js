bfdffff03f78f1da2947ec1b0a7e69b6461e5435
Ethan Szeto <ethan.szeto@gmail.com>
2023-12-24 00:06:45 -0500

    rudimentary article views created.
    Supports mixing up of body paragraphs and pull quotes sections.
    
    In elementOrder, it must be defined as a string "body" or "pull" to denote the type of section

--- api/routes/articles.route.js
+++ api/routes/articles.route.js
@@ -1,14 +1,10 @@
-/** REQUIRES REWORK */
-
 import express from "express";
 import ArticlesCTRL from "../controllers/articles.controller.js";
-import catchError from "../error/error.handler.js";
 
 /* Controls Routing for Finished Articles */
 
 const router = express.Router();
 
-//any valid ID of an article should work... ID or NAME perhaps
 router.route("/:id").get(ArticlesCTRL.apiGetArticle);
 
 router.route("/").get(ArticlesCTRL.apiGetArticles);
