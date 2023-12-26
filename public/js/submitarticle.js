8578c349e724d1fb0377531fe6597cabb4fb026e
Ethan Szeto <ethan.szeto@gmail.com>
2023-12-25 19:14:41 -0500

    Added more comments, Added functions to try and send the proper and necessary data up to the route

--- public/js/submitarticle.js
+++ public/js/submitarticle.js
@@ -16,20 +16,30 @@ window.addEventListener("load", () => {
 });
 
 function generateElement(type, text, index) {
+  //textarea
   let textarea = document.createElement("textarea");
+  textarea.id = `textarea-${index}`;
   textarea.classList.add("input-text", type);
   textarea.placeholder = type;
   textarea.innerHTML = text;
+
+  //div to contain the textarea
   let div = document.createElement("div");
   div.classList.add("details");
   div.appendChild(textarea);
+
+  //delete image icon
   let deleteImg = document.createElement("img");
   deleteImg.id = `delete-item-${index}`;
   deleteImg.classList.add("ui-delete-element");
   deleteImg.src = "/public/assets/trashcan.webp";
+
+  //draggable dots icon
   let dragImg = document.createElement("img");
   dragImg.classList.add("ui-draggable-dots");
   dragImg.src = "/public/assets/draggabledots.webp";
+
+  //list element object to contain all
   let li = document.createElement("li");
   li.classList.add("item");
   li.draggable = true;
@@ -37,4 +47,36 @@ function generateElement(type, text, index) {
   li.appendChild(deleteImg);
   li.appendChild(dragImg);
   return li;
+}
+
+//may want to abstract in the future when there are more dynamic text-based elements
+function getAllBodyParagraphs() {
+  const bodyElements = document.querySelectorAll(".body");
+  const bodyParagraphs = [];
+  for (let bp of bodyElements) {
+    bodyParagraphs.push(bp.innerHTML);
+  }
+  return bodyParagraphs;
+}
+
+function getAllPullQuotes() {
+  const pullElements = document.querySelectorAll(".pull");
+  const pullQuotes = [];
+  for (let pq of pullElements) {
+    pullQuotes.push(pq.innerHTML);
+  }
+  return pullQuotes;
+}
+
+function getOrder() {
+  const elements = document.querySelectorAll(".body", ".pull");
+  const order = [];
+  for (let e of elements) {
+    if (e.classList.contains("body")) {
+      order.push("body");
+    } else if (e.classList.contains("pull")) {
+      order.push("pull");
+    }
+  }
+  return order;
 }
