0cf3fb71e4e372af056948c393b9ba06e5e9f4ea
benniu04 <nub38bn@gmail.com>
2024-04-09 17:21:34 -0400

    Update articles.accessor.js

--- api/database_accessor/articles.accessor.js
+++ api/database_accessor/articles.accessor.js
@@ -100,7 +100,44 @@ export default class ArticlesAccessor {
     try {
       await Connection.open("articles");
       const article = await Article.findById(new mongoose.Types.ObjectId(articleID));
-      return article;
+      console.log('Article: ', article);
+      return article
+    } catch (e) {
+      console.error(e);
+      throw e;
+    }
+  }
+
+  /**
+   * sortArticles method
+   * 
+   * @param {String} sortBy - The field to sort by, defaulting to Date.
+   * @param {String} order - The order of sorting, 'asc' for ascending and 'desc' for descending, defaulting to 'asc'.
+   * @param {Number} limit - The maximum number of articles allowed per page
+   * @param {Number} page - The page number
+   * @returns {Array} - returns an array of the sorted articles
+   */
+  static async sortArticles({
+    sortBy = Date, // Date is from article.js
+    order = 'asc', 
+    limit = 10,
+    page = 1
+  } = {}){
+    try{
+      await Connection.open("articles");
+      let sortOrder;
+      if (order =='asc'){
+        sortOrder = 1;
+      } else {
+        sortOrder = -1;
+      }
+      const skip = (page - 1) * limit;
+      const articles = await Article.find({}).sort({ [sortBy]: sortOrder}) // Sort the articles by the specified field and order
+      .skip(skip) 
+      .limit(limit); // Limit the number of returned documents to the specified limit
+    
+      return articles;
+    
     } catch (e) {
       console.error(e);
       throw e;
