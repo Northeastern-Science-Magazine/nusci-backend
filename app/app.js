9c7076008f110edfacf200a2f81fd38fe07d3832
Ethan Szeto <ethan.szeto@gmail.com>
2024-11-03 19:44:26 -0500

    Bumped Express to v5
    Renamed routers to be more explicit which router you're on

--- app/app.js
+++ app/app.js
@@ -1,9 +1,8 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
-import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import articleRouter from "./routes/articleRoutes.js";
+import articlesRouter from "./routes/articleRoutes.js";
 import calendarRouter from "./routes/calendarRoutes.js";
 import defaultRouter from "./routes/defaultRoutes.js";
 import issueMapRouter from "./routes/issueMapRoutes.js";
@@ -23,10 +22,9 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser.json());
 
 app.use("/", defaultRouter);
-app.use("/articles", articleRouter);
+app.use("/articles", articlesRouter);
 app.use("/calendar", calendarRouter);
 app.use("/issue-map", issueMapRouter);
 app.use("/photo", photoRouter);
--- app/routes/calendarRoutes.js
+++ app/routes/calendarRoutes.js
@@ -1,8 +1,5 @@
 import express from "express";
-import bodyParser from "body-parser";
 
 const router = express.Router();
-
-router.use(bodyParser.urlencoded({ extended: false }));
 
 export default router;
