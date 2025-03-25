5790c18ec481b7c817ed80cf76f72d906fdbcd40
yurikakan <kan.yu@northeastern.edu>
2025-03-25 03:15:38 -0400

    new location

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // express app
 import app from "./app/app.js";
 
+/**
+ * This file is the project entry point.
+ */
+
+const { SERVER_HOSTNAME, SERVER_PORT } = process.env;
+
 server.listen(SERVER_PORT, () => {
   console.log(`Server running at http://${SERVER_HOSTNAME}:${SERVER_PORT}/`);
 });
