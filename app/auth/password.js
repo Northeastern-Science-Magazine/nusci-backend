c4ccc3d536715b78acdd73aa5d4223518f3a4bd3
stephanie <stephaniechen028@gmail.com>
2025-03-26 19:31:58 -0400

    comments

--- app/auth/password.js
+++ app/auth/password.js
@@ -9,12 +9,29 @@ import bcrypt from "bcryptjs";
  */
 export class Password {
 
+  /**
+   * hash method
+   *
+   * Takes in a password (string) and return a hashed password.
+   *
+   * @param {String} password
+   * @returns {String} encrypted 
+   */    
   static async hash(password) {
     const encrypted = await bcrypt.hash(password, 10);
     return encrypted;
   }
 
-
+  /**
+   * compare method 
+   *
+   * Takes in a 'trial' and the encrypted password, and 
+   * returns true or false whether or not it is a valid password.
+   * 
+   * @param {String} trial 
+   * @param {String} encrypted 
+   * @returns {boolean} valid 
+   */
   static async compare(trial, encrypted) {
     const valid = await bcrypt.compare(trial, encrypted);
     return valid;
