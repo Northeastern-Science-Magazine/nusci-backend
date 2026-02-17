993a704b0865f25ec6fa30167a6784b6236411c8
Ethan Szeto <ethan.szeto@gmail.com>
2026-02-17 14:03:34 -0500

    Update categories.js

--- app/models/enums/categories.js
+++ app/models/enums/categories.js
@@ -4,24 +4,24 @@ import { ErrorValidation } from "../../error/errors.js";
  * Enumerated Class for Article Categories
  */
 export default class Category {
-  static Biology = new Category("biology");
-  static Chemistry = new Category("chemistry");
-  static Culture = new Category("culture");
-  static Environment = new Category("environment");
-  static Health = new Category("health");
-  static Local = new Category("local");
-  static Mathematics = new Category("mathematics");
-  static Neuroscience = new Category("neuroscience");
-  static Newsletter = new Category("newsletter");
-  static Opinion = new Category("opinion");
-  static Philosophy = new Category("philosophy");
-  static Physics = new Category("physics");
-  static Politics = new Category("politics");
-  static Psychology = new Category("psychology");
-  static Space = new Category("space");
-  static Technology = new Category("technology");
-  static Uncategorized = new Category("uncategorized");
-  static World = new Category("world");
+  static Biology = new Category("Biology");
+  static Chemistry = new Category("Chemistry");
+  static Culture = new Category("Culture");
+  static Environment = new Category("Environment");
+  static Health = new Category("Health");
+  static Local = new Category("Local");
+  static Mathematics = new Category("Mathematics");
+  static Neuroscience = new Category("Neuroscience");
+  static Newsletter = new Category("Newsletter");
+  static Opinion = new Category("Opinion");
+  static Philosophy = new Category("Philosophy");
+  static Physics = new Category("Physics");
+  static Politics = new Category("Politics");
+  static Psychology = new Category("Psychology");
+  static Space = new Category("Space");
+  static Technology = new Category("Technology");
+  static Uncategorized = new Category("Uncategorized");
+  static World = new Category("World");
 
   /**
    * INTERNAL USE ONLY
@@ -49,7 +49,7 @@ export default class Category {
    * @returns {Cateogory}
    */
   static toCategory(str) {
-    const category = this.list().find(obj => obj.toString() === str.toLowerCase());
+    const category = this.list().find((obj) => obj.toString() === str.toLowerCase());
     if (!category) {
       throw new ErrorValidation("Invalid Category enum given.");
     }
