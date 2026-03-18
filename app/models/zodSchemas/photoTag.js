7737fbe4c770f17d6e04ce6f44855421e6f121b4
adrodvaz04 <isyla.adrian@gmail.com>
2026-03-18 11:40:48 -0400

    pre-testing implementation of issuemap, phototag, user schemas

--- app/models/zodSchemas/photoTag.js
+++ app/models/zodSchemas/photoTag.js
@@ -1,5 +1,5 @@
 import * as z from "zod";
-import { UserPublicResponse } from "./user";
+import { UserPublicResponse } from "./user.js";
 
 export const PhotoTag = z.object({
     tagName: z.string(),
@@ -9,10 +9,10 @@ export const PhotoTag = z.object({
     modificationTime: z.date().default(new Date())
 });
 
+export const PhotoTagCreate = PhotoTag;
+
 export const PhotoTagResponse = z.object({
-    tagName: z.string().refine((tag) => {
-        // logic for checking uniqueness
-    }),
+    tagName: z.string(),
     color: z.string(),
     creatingUser: UserPublicResponse,
     creationTime: z.date(),
