036d7ea43e29f9b8bc8ea427b077f60fec07f655
adrodvaz04 <isyla.adrian@gmail.com>
2026-03-18 22:48:57 -0400

    refactor: set photo tag audit fields to string type

--- app/models/zodSchemas/photoTag.js
+++ app/models/zodSchemas/photoTag.js
@@ -5,8 +5,9 @@ export const PhotoTag = z.object({
     tagName: z.string(),
     color: z.string(),
     creatingUser: z.string(),
-    creationTime: z.date().default(new Date()),
-    modificationTime: z.date().default(new Date())
+    // test data shows audit fields as strings, z.date() doesn't seem to recognize it.
+    creationTime: z.string().default(new Date()),
+    modificationTime: z.string().default(new Date())
 });
 
 export const PhotoTagCreate = PhotoTag;
